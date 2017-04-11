d34e4a962e7f0e9a95730f1b3527ad613ca6b045
mezd10 <sn_kh5@mail.ru>
2017-04-11 12:45:06 +0300

    прибрался в репозитории

--- test/HumanReaderSizeTest.java
+++ test/HumanReaderSizeTest.java
@@ -9,7 +9,10 @@ public class HumanReaderSizeTest {
         long size = new HumanReaderSize("progr").size();
         assertEquals(153, size);// тестирование файла
 
-        long sizeDirectory = new HumanReaderSize("progrDirectory").size();
-        assertEquals(799, sizeDirectory);// тестирование папки
+        long sizeDirectoryInDirectory = new HumanReaderSize("progrDirectory").size();
+        assertEquals(1741, sizeDirectoryInDirectory);// тестирование директории, в которой есть вложенная директория + несколько файлов
+
+        long sizeDirectory = new HumanReaderSize("exemple1").size();
+        assertEquals(942, sizeDirectory);// тестирование директория
     }
 }
